e5377dc5e55bd959911a61bd544cf8d465fdd425
iipythonix <79859739+iipythonix@users.noreply.github.com>
2021-04-09 19:20:39 -0400

    Add files via upload

---
+++ RaftGUI/RaftGUI/Main1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using FMODUnity;
+using Console = System.Console;
+using Object = UnityEngine.Object;
+using String = System.String;
+
+namespace RaftGUI
+{
+    public class Main1 : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Debug.Log("Set console title to: Raft");
+            Console.Title = string.Format("Raft");
+        }
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                MelonLogger.Msg("You just pressed T");
+            }
+        }
+    }
+}
+
+public class ESP
+{
+    
+}
